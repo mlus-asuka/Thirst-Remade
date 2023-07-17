42f566d969ed113c208f01f9d02074c69f13f4dd
mlus-Asuka <1319237806@qq.com>
2023-07-17 23:35:29 +0800

    add cactus recipe.

--- src/main/java/vip/fubuki/thirst/foundation/config/KeyWordConfig.java
+++ src/main/java/vip/fubuki/thirst/foundation/config/KeyWordConfig.java
@@ -24,25 +24,25 @@ public class KeyWordConfig {
         BUILDER.push("BlackList Keyword")
                 .comment("Defines items will be ignored if it contains the keyword",
                         "Format: [(keyword1|keyword2|keyword3)]");
-        KEYWORD_BLACKLIST = BUILDER.define("keyword_blacklist", "(dried|candied|leaf|leaves|gummy|crate|jam|sauce|bucket|seed|cookie|pie|bush|sapling|bean|curry|cake|candy)");
+        KEYWORD_BLACKLIST = BUILDER.define("keyword_blacklist", "(?:\\b|[^a-zA-Z])(dried|candied|leaf|leaves|gummy|crate|jam|sauce|bucket|seed|cookie|pie|bush|sapling|bean|curry|cake|candy)(?:\\b|[^a-zA-Z])");
         BUILDER.pop();
 
         BUILDER.push("Drink KeyWord")
                 .comment("Defines items will be considered as drink if it contains the keyword",
                         "Format: [(keyword1|keyword2|keyword3)]");
-        KEYWORD_DRINK = BUILDER.define("keyword_drink", "(drink|juice|tea|soda|coffee|wine|beer|cider|yogurt|milkshake|smoothie)");
+        KEYWORD_DRINK = BUILDER.define("keyword_drink", "(?:\\b|[^a-zA-Z])(drink|juice|tea|soda|coffee|wine|beer|cider|yogurt|milkshake|smoothie)(?:\\b|[^a-zA-Z])");
         BUILDER.pop();
 
         BUILDER.push("Soup KeyWord")
                 .comment("Defines items will be considered as soup if it contains the keyword",
                         "Format: [(keyword1|keyword2|keyword3)]");
-        KEYWORD_SOUP = BUILDER.define("keyword_soup", "(soup|stew|porridge)");
+        KEYWORD_SOUP = BUILDER.define("keyword_soup", "(?:\\b|[^a-zA-Z])(soup|stew|porridge)(?:\\b|[^a-zA-Z])");
         BUILDER.pop();
 
         BUILDER.push("Fruit KeyWord")
                 .comment("Defines items will be considered as fruit if it contains the keyword",
                         "Format: [(keyword1|keyword2|keyword3)]");
-        KEYWORD_FRUIT = BUILDER.define("keyword_fruit", "(fruit|berry|berries|grape|orange|peach|pear|coconut|lemon|melon|cherry|apple)");
+        KEYWORD_FRUIT = BUILDER.define("keyword_fruit", "(?:\\b|[^a-zA-Z])(fruit|berry|berries|grape|orange|peach|pear|coconut|lemon|melon|cherry|apple)(?:\\b|[^a-zA-Z])");
         BUILDER.pop();
 
         SPEC = BUILDER.build();
